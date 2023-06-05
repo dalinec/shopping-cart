bedfb74d0a1ca348e544e6a01eaeeb2bb0838dff
dalinec <dalinec123@gmail.com>
2023-06-05 13:27:19 +0200

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className='flex justify-center mt-10'>
+        <h1 className='text-yellow-400 text-5xl'>Hello</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
